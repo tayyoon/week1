cd49b1608824e2f0352d1f5bcfde373e128c3122
moonhjang <99785621+moonhjang@users.noreply.github.com>
2022-03-08 15:21:33 +0900

    회원가입_기능구현 완료

--- app_membership.py
+++ app_membership.py
@@ -1,12 +1,17 @@
 from flask import Flask, render_template, jsonify, request, session, redirect, url_for
+import jwt  # PYJWT 설치
+import datetime
+import hashlib  # 회원가입 시, 비밀번호를 암호화하여 DB에 저장
 
 app = Flask(__name__)
+
 
 # 서버연결1
 # from pymongo import MongoClient
 #
-# client = MongoClient('mongodb://3.34.44.93', 27017, username="sparta", password="woowa")
-# db = client.dbsparta_plus_week4
+# client = MongoClient("mongodb+srv://test:sparta@cluster0.qgh8x.mongodb.net/Cluster0?retryWrites=true&w=majority")
+# db = client.dbsparta
+
 
 # 서버연결2 (문희)
 import pymongo
@@ -17,28 +22,12 @@ client = pymongo.MongoClient("mongodb+srv://test:sparta@cluster0.qgh8x.mongodb.n
 db = client.dbsparta
 
 
-# JWT 토큰을 만들 때 필요한 비밀문자열입니다. 아무거나 입력해도 괜찮습니다.
-# 이 문자열은 서버만 알고있기 때문에, 내 서버에서만 토큰을 인코딩(=만들기)/디코딩(=풀기) 할 수 있습니다.
 SECRET_KEY = 'SPARTA'
 
-# JWT 패키지를 사용합니다. (설치해야할 패키지 이름: PyJWT)
-import jwt
 
-# 토큰에 만료시간을 줘야하기 때문에, datetime 모듈도 사용합니다.
-import datetime
-
-# 회원가입 시엔, 비밀번호를 암호화하여 DB에 저장해두는 게 좋습니다.
-# 그렇지 않으면, 개발자(=나)가 회원들의 비밀번호를 볼 수 있으니까요.^^;
-import hashlib
-
-
-#################################
-##  HTML을 주는 부분             ##
-#################################
 @app.route('/')
 def home():
     return render_template('membership.html')
-    #
     # token_receive = request.cookies.get('mytoken')
     # try:
     #     payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
@@ -52,16 +41,12 @@ def home():
 
 @app.route('/login')
 def login():
-    # msg = request.args.get("msg")
-    # return render_template('membership.html', msg=msg)
-    return render_template('membership.html')
-
+    msg = request.args.get("msg")
+    return render_template('membership.html', msg=msg)
 
 @app.route('/register')
 def register():
     return render_template('membership.html')
-
-
 
 
 #################################
@@ -72,18 +57,28 @@ def register():
 # id, pw, nickname을 받아서, mongoDB에 저장합니다.
 # 저장하기 전에, pw를 sha256 방법(=단방향 암호화. 풀어볼 수 없음)으로 암호화해서 저장합니다.
 
-@app.route('/api/register', methods=['POST'])
-def api_register():
-    id_receive = request.form['id_give']
-    pw_receive = request.form['pw_give']
+@app.route('/sign_up/save', methods=['POST'])
+def sign_up():
+    username_receive = request.form['username_give']
+    password_receive = request.form['password_give']
     nickname_receive = request.form['nickname_give']
 
-    pw_hash = hashlib.sha256(pw_receive.encode('utf-8')).hexdigest()
-
-    save = db.user.insert_one({'id': id_receive, 'pw': pw_hash, 'nick': nickname_receive})
-    print(save)
+    print(username_receive)
+    password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    doc = {
+        "username": username_receive,                               # 아이디
+        "password": password_hash,                                  # 비밀번호
+        "nickname": nickname_receive,                               # 닉네임
+     }
+    db.user.insert_one(doc)
     return jsonify({'result': 'success'})
 
+
+@app.route('/sign_up/check_dup', methods=['POST'])
+def check_dup():
+    username_receive = request.form['username_give']
+    exists = bool(db.users.find_one({"username": username_receive}))
+    return jsonify({'result': 'success', 'exists': exists})
 
 #
 # # [로그인 API]
@@ -146,28 +141,5 @@ def api_register():
 #         return jsonify({'result': 'fail', 'msg': '로그인 정보가 존재하지 않습니다.'})
 #
 #
-
-# @app.route('/sign_up/save', methods=['POST'])
-# def sign_up():
-#     username_receive = request.form['username_give']
-#     password_receive = request.form['password_give']
-#     password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
-#     doc = {
-#         "username": username_receive,                               # 아이디
-#         "password": password_hash,                                  # 비밀번호
-#         "profile_name": username_receive,                           # 프로필 이름 기본값은 아이디
-#         "profile_pic": "",                                          # 프로필 사진 파일 이름
-#         "profile_pic_real": "profile_pics/profile_placeholder.png", # 프로필 사진 기본 이미지
-#         "profile_info": ""                                          # 프로필 한 마디
-#     }
-#     db.users.insert_one(doc)
-#     return jsonify({'result': 'success'})
-
-@app.route('/sign_up/check_dup', methods=['POST'])
-def check_dup():
-    username_receive = request.form['username_give']
-    exists = bool(db.users.find_one({"username": username_receive}))
-    return jsonify({'result': 'success', 'exists': exists})
-
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
